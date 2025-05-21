370ee576d08600431ec673230e2c358cf89e5624
Jay Mavani <jaymavanime39@gmail.com>
2025-05-21 12:46:09 +0530

    This is a first commit of cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project for practicing end to end ML emotion detection using mlflow and dagshub experiment tracking and than model serving using flask.',
+    author='Jay Mavani',
+    license='',
+)
