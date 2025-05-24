376ee815f2462afc5aac7b585ed65c77d95cbcb6
Jay Mavani <jaymavanime39@gmail.com>
2025-05-24 11:20:00 +0530

    Solving error related to model registry and than adding version 1 model in 'staging'

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -55,21 +55,22 @@ def load_model_info(file_path: str) -> dict:
         logger.error('Unexpected error occurred while loading the model info: %s', e)
         raise
 
-def register_model(model_name: str,model_info: dict):
-    """Register the model to mlflow model registery"""
+def register_model(model_name: str, model_info: dict):
+    """Register the model to the MLflow Model Registry."""
     try:
-        model_uri:f"runs/:{model_info['run_id']/{model_info['model_path']}}"
-
-        # register the model
-        model_version = mlflow.register_model(model_uri,model_name)
-
-        # transition the model to "staging" in model stage
+        model_uri = f"runs:/{model_info['run_id']}/{model_info['model_path']}"
+        
+        # Register the model
+        model_version = mlflow.register_model(model_uri, model_name)
+        
+        # Transition the model to "Staging" stage
         client = mlflow.tracking.MlflowClient()
         client.transition_model_version_stage(
             name=model_name,
             version=model_version.version,
             stage="Staging"
         )
+        
         logger.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
     except Exception as e:
         logger.error('Error during model registration: %s', e)
@@ -77,14 +78,14 @@ def register_model(model_name: str,model_info: dict):
 
 def main():
     try:
-        model_name = 'My model'
         model_info_path = 'reports/experiment_info.json'
         model_info = load_model_info(model_info_path)
-        register_model(model_name,model_info)
-
+        
+        model_name = "my_model"
+        register_model(model_name, model_info)
     except Exception as e:
         logger.error('Failed to complete the model registration process: %s', e)
-        print(f"Error: {e}") 
+        print(f"Error: {e}")
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
